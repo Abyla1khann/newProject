48228287c167d666875d0b051cc7d2fdc1f19ebd
Abyla1khann <220103117@stu.sdu.edu.kz>
2022-11-22 16:29:48 +0600

    I've added add.java file

---
+++ add.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner las=new Scanner(System.in);
+        int a= las.nextInt();
+        int n=0;
+        while(a!=0){
+            n+=a%10;
+            a/=10;
+
+
+        }
+        System.out.println(a);
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
